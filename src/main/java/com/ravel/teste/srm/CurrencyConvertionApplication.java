679060ad52eb4dcdb45df2e78a7a45622a418e8e
ravelmello <ravelmello3@gmail.com>
2025-05-30 16:25:56 -0300

    [WIP] Spring initlzr archives

---
+++ src/main/java/com/ravel/teste/srm/CurrencyConvertionApplication.java
@@ -0,0 +1,13 @@
+package com.ravel.teste.srm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConvertionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConvertionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ravel/teste/srm/CurrencyConvertionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravel.teste.srm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConvertionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
