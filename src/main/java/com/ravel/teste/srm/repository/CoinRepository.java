627b2ddddb448eb1323b43a1fd5c6d4bebdd21c2
ravelmello <ravelmello3@gmail.com>
2025-05-30 16:25:00 -0300

    [WIP] First repo adiction

---
+++ src/main/java/com/ravel/teste/srm/repository/CoinRepository.java
@@ -0,0 +1,10 @@
+package com.ravel.teste.srm.repository;
+
+import com.ravel.teste.srm.entity.Coin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CoinRepository extends JpaRepository<Coin, Long> {
+    Optional<Coin> findByName(String name);
+}
---
+++ src/main/java/com/ravel/teste/srm/repository/ExchangeTaxRepository.java
@@ -0,0 +1,8 @@
+package com.ravel.teste.srm.repository;
+
+import com.ravel.teste.srm.entity.ExchangeTax;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExchangeTaxRepository extends JpaRepository<ExchangeTax, Long> {
+
+}
